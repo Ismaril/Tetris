846c30a6a0fb2ac04273d30d8bb6919ac847c351
tomlazen <oismarilo@gmail.com>
2023-08-27 11:44:48 +0200

    first commit

---
+++ Music.cs
@@ -0,0 +1,128 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Music
+    {
+        static readonly string path = @"../../Music/";
+
+        static readonly string soundRotate = path + @"SFX 6.mp3";
+        static readonly string soundMoveToSides = path + @"SFX 4.mp3";
+        static readonly string soundGameOver = path + @"SFX 14.mp3";
+
+        static readonly string soundObstacle = path + @"SFX 8.wav";
+        static readonly string soundLineCleared = path + @"SFX 11.wav";
+        static readonly string soundTetris = path + @"SFX TetrisClear.wav";
+        static readonly string soundNextLevel = path + @"SFX 7.wav";
+
+        static readonly string backgroudMusic1_Slow = path + @"1 - Music 1.mp3";
+        static readonly string backgroudMusic2_Slow = path + @"2 - Music 2.mp3";
+        static readonly string backgroudMusic3_Slow = path + @"3 - Music 3.mp3";
+
+        static readonly string backgroudMusic1_Fast = path + @"8 - Music 8.mp3";
+        static readonly string backgroudMusic2_Fast = path + @"9 - Music 9.mp3";
+        static readonly string backgroudMusic3_Fast = path + @"10 - Music 10.mp3";
+
+        WaveOut rotateUserEvent = new WaveOut();
+        WaveOut moveToSidesUserEvent = new WaveOut();
+        WaveOutEvent backgroundMusicPlayer = new WaveOutEvent();
+        WaveOut gameOver = new WaveOut();
+
+        SoundPlayer obstacle = new SoundPlayer(soundObstacle);
+        SoundPlayer lineCleared = new SoundPlayer(soundLineCleared);
+        SoundPlayer tetris = new SoundPlayer(soundTetris);
+        SoundPlayer nextLevel = new SoundPlayer(soundNextLevel);
+
+
+        //WaveOut obstacle = new WaveOut();
+        //WaveOut lineCleared = new WaveOut();
+        //WaveOut tetris = new WaveOut();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Music() { }
+
+        public void MoveToSides()
+        {
+            AudioFileReader address = new AudioFileReader(soundMoveToSides);
+            moveToSidesUserEvent.Init(address);
+            moveToSidesUserEvent.Play();
+        }
+
+        public void Rotate()
+        {
+            AudioFileReader address = new AudioFileReader(soundRotate);
+            rotateUserEvent.Init(address);
+            rotateUserEvent.Play();
+        }
+
+        public void MainMusic()
+        {
+            // Initialize the background music player
+            AudioFileReader backgroundMusicReader = new AudioFileReader(backgroudMusic1_Slow);
+            // Start playing the background music (looped)
+            backgroundMusicPlayer.Init(backgroundMusicReader);
+            backgroundMusicPlayer.Play();
+        }
+
+        public void NextLevel()
+        {
+            nextLevel.Play();
+        }
+
+        public void Obstacle()
+        {
+            //AudioFileReader address = new AudioFileReader(soundObstacle);
+            //obstacle.Init(address);
+            obstacle.Play();
+        }
+
+        public void LineCleared()
+        {
+            //AudioFileReader address = new AudioFileReader(soundLineCleared);
+            //lineCleared.Init(address);
+            lineCleared.Play();
+        }
+        public void GameOver()
+        {
+            AudioFileReader address = new AudioFileReader(soundGameOver);
+            gameOver.Init(address);
+            gameOver.Play();
+        }
+
+        public void Tetris()
+        {
+        //    AudioFileReader address = new AudioFileReader(soundTetris);
+        //    tetris.Init(address);
+            tetris.Play();
+        }
+
+        /// <summary>
+        /// Free up memory resources.
+        /// </summary>
+        public void DisposeSFX_NAudio()
+        {
+            rotateUserEvent.Dispose();
+            moveToSidesUserEvent.Dispose();
+            //obstacle.DisposeSFX_NAudio();
+            //lineCleared.DisposeSFX_NAudio();
+            gameOver.Dispose();
+            //tetris.DisposeSFX_NAudio();
+        }
+
+        /// <summary>
+        /// Free up memory resources.
+        /// </summary>
+        public void DisposeBackgroundMusic_NAudio()
+        {
+            backgroundMusicPlayer.Dispose();
+        }
+    }
+}
---
+++ Sprites.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Sprites
+    {
+        private List<List<Image>> tetrominoSpriteCollection = new List<List<Image>>();
+
+        public static readonly string pathToSprites = @"../../Sprites/";
+
+        public readonly Image GRID_COLOR = Image.FromFile(pathToSprites + @"grid.png");
+        public readonly Image OFFGRID_COLOR = Image.FromFile(pathToSprites + @"offgrid.png");
+
+        public static readonly Image BLUE_WHITE = Image.FromFile(pathToSprites + @"blue_white.png");
+        public static readonly Image BLUE = Image.FromFile(pathToSprites + @"blue.png");
+        public static readonly Image TURQUOISE = Image.FromFile(pathToSprites + @"turquoise.png");
+        public static readonly Image TURQUOISE_WHITE_BLUE = Image.FromFile(pathToSprites + @"turquoise_white_blue.png");
+
+        public static readonly Image DARK_GREEN_WHITE = Image.FromFile(pathToSprites + @"dark_green_white.png");
+        public static readonly Image DARK_GREEN = Image.FromFile(pathToSprites + @"dark_green.png");
+        public static readonly Image PASTEL_GREEN = Image.FromFile(pathToSprites + @"pastel_green.png");
+        public static readonly Image PASTEL_GREEN_WHITE_DARK_GREEN = Image.FromFile(pathToSprites + @"pastel_green_white_dark_green.png");
+
+        public List<List<Image>> TetrominoSpriteCollection { get => tetrominoSpriteCollection;}
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Sprites()
+        {
+            TetrominoSpriteCollection.Add(new List<Image> { BLUE_WHITE, BLUE, TURQUOISE, TURQUOISE_WHITE_BLUE });
+            TetrominoSpriteCollection.Add(new List<Image> { DARK_GREEN_WHITE, DARK_GREEN, PASTEL_GREEN, PASTEL_GREEN_WHITE_DARK_GREEN });
+
+        }
+
+    }
+}
